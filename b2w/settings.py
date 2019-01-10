7b6f67e60c42515ca073a4a34db7e8ac2c3acd96
pedro nascimento <pedrotnascimento@gmail.com>
2019-01-10 01:09:58 -0200

    adicionado mongoDB como engine(djongo)

--- b2w/settings.py
+++ b2w/settings.py
@@ -77,9 +77,11 @@ WSGI_APPLICATION = 'b2w.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME':  'b2wDB' # 'mongodb://localhost:27017',
     }
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
 
 
